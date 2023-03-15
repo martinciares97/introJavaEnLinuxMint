759dec810ad1851b29a49c07071c55a60461bec0
Martin <m.ciares97@gmail.com>
2023-03-15 18:55:43 -0300

    guia4ejercicioResueltos

--- backendIntroJava01/src/guia04/ejercicio04.java
+++ backendIntroJava01/src/guia04/ejercicio04.java
@@ -5,6 +5,8 @@
  */
 package guia04;
 
+import java.util.Scanner;
+
 /**
  *
  * @author cuni
@@ -15,9 +17,30 @@ public class ejercicio04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero");
+        int num = sc.nextInt();
+
+        verificarNumPrimo(num);
     }
-    
+
+    public static void verificarNumPrimo(int num) {
+        int cont = 0;
+
+        if (num >= 2) {
+            for (int i = 2; i < num + 1; i++) {
+                if (num % i == 0) {
+                    cont++;
+                }
+            }
+            if (cont > 1) {
+                System.out.println("El numero no es primo");
+            } else {
+                System.out.println("El numero es primo");
+            }
+        }
+    }
 }
 /*
 4. Crea una aplicación que nos pida un número por teclado y con una
